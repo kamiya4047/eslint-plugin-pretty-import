1f88e40de2943abfe76247489467e242fb2cb401
Kamiya <kamiya@kamiya.app>
2026-04-30 11:36:47 +0800

    fix: dts build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,42 @@
-import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'es',
-    sourcemap: true
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.js',
+      format: 'es',
+      sourcemap: false,
+    },
+    external: [
+      '@typescript-eslint/types',
+      'eslint',
+      'node:path',
+      'node:fs',
+    ],
+    plugins: [
+      json(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: 'dist/temp',
+        outDir: 'dist',
+        include: ['src/**/*', '!src/test-utils.ts'],
+      }),
+    ],
   },
-  external: [
-    '@typescript-eslint/types',
-    '@typescript-eslint/utils',
-    'eslint'
-  ],
-  plugins: [
-    json(),
-    typescript({
-      tsconfig: './tsconfig.json',
-      declaration: true,
-      declarationDir: 'dist',
-      outDir: 'dist',
-      include: ['src/**/*', '!src/test-utils.ts']
-    })
-  ]
-};
+  {
+    input: 'dist/temp/src/index.d.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es',
+    },
+    external: [
+      '@typescript-eslint/types',
+      'eslint',
+    ],
+    plugins: [dts()],
+  },
+];
